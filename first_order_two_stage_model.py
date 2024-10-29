dd7b181dcc22872ad73ffe1b7599430ff4df3fb9
Georg Eichler <georg_eichler@web.de>
2024-10-29 14:15:13 +0000

    Created a separate file for Michaelis-Menten kinetics model

--- first_order_two_stage_model.py
+++ first_order_two_stage_model.py
@@ -45,30 +45,6 @@ def simulate_two_stage_ode(y0, B, a, k, tau, dose_times, end_time):
     min_concentration = minima
     return t, b, s, min_concentration, max_concentration
 
-def two_stage_Michaelis_Menten_ode(t, y, B, a, k, km):
-    b, s = y
-    dbdt = B*a*s - k/(km + b)*b
-    dsdt = -a*s
-
-    return [dbdt, dsdt]
-
-def simulate_two_stage_Michaelis_Menten_ode(y0, D, B, a, k, km, dose_times, end_time):
-    t = np.array([])
-    b = np.array([])
-    s = np.array([])
-    
-    times = np.concatenate(([0], dose_times, [end_time] ))
-    
-    for i in range(len(times) - 1):
-        t_eval = np.linspace(times[i], times[i+1], 1001)
-
-        sol = solve_ivp(two_stage_Michaelis_Menten_ode, (times[i], times[i+1]), y0, t_eval=t_eval, args=(B, a, k, km))
-        t = np.concatenate((t, sol.t))
-        b = np.concatenate((b, sol.y[0]))
-        s = np.concatenate((s, sol.y[1]))
-        y0 = [b[-1], s[-1] + D] #get new initial value and apply pulse for new pill, 1 is nondimensionalized dose
-
-    return t, b, s
 
 T12 = 1.5         #half-life of antibiotica in [h]
 k = np.log(2)/T12 #degredation rate of antibiotica in [1/h]
